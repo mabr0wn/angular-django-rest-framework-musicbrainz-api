9dff2848b61cab80286f0b23688ce2a450c94504
Matthew Brown <mattd429@gmail.com>
2017-10-16 16:33:14 -0400

    Update test_views.py

--- experiments/tests/test_views.py
+++ experiments/tests/test_views.py
@@ -1,1 +1,8 @@
+import djago.test import TestCase
+from .models import Experiment
+from .views import ExperimentList, ExperimentDetail
 
+class ExperimentTestCase(TestCase):
+    def setup(self):
+        pass
+    
