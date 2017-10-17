51252b36f349f994b390e9bb914326ac83cf0281
Matthew Brown <mattd429@gmail.com>
2017-10-17 14:06:52 -0400

    Update urls.py

--- experiments/urls.py
+++ experiments/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
 
 # User URLs
 urlpatterns += [
-	url(r'^users/'$, views.UserList.as_view()),
+	url(r'^users/$', views.UserList.as_view()),
 	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
