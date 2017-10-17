839ccd02bddee7817652b7083ddec9a05620f1cf
Matthew Brown <mattd429@gmail.com>
2017-10-17 14:06:18 -0400

    Update urls.py
    added the user urls.

--- experiments/urls.py
+++ experiments/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 from experiments import views
 
+# Experiments URLs
 urlpatterns = [
 	url(r'^experiments/$', views.ExperimentList.as_view()),
 	url(r'^experiments/(?P<pk>[0-9]+)/$', views.ExperimentDetail.as_view()),
 
+]
+
+# User URLs
+urlpatterns += [
+	url(r'^users/'$, views.UserList.as_view()),
+	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
