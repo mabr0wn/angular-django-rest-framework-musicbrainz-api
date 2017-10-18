34ef835e32a175ab1bc6a85a0bb48c543f59439f
Matthew Brown <mattd429@gmail.com>
2017-10-18 10:01:12 -0400

    Update urls.py
    did some changes to the URLs put all urls in one pattern since they are all API endpoints.  api-auth is in a separate url since it is a login and logout authorization.

--- experiments/urls.py
+++ experiments/urls.py
@@ -2,18 +2,17 @@ from django.conf.urls import url, include
 from rest_framework.urlpattersn import format_suffix_patterns
 from experiments import views
 
-# Experiments URLs
-urlpatterns = [
-	url(r'^experiments/$', views.ExperimentList.as_view()),
-	url(r'^experiments/(?P<pk>[0-9]+)/$', views.ExperimentDetail.as_view()),
+# API endpoints
+# Create format suffixes patterns for our URLs
+urlpatterns = format_suffix_patterns([
+	url(r'^$', views.api_root),
+	url(r'^experiments/$', views.ExperimentList.as_view(), name='experiment-list'),
+	url(r'^experiments/(?P<pk>[0-9]+)/$', views.ExperimentDetail.as_view(), name='experiment-detail'),
+	url(r'^experiments/(?P<pk>[0-9]+)/hightlight/$', views.ExperimentHighlight.as_view(), name='experiment-highlight'),
+	url(r'users/$', views.UserList.as_view(), name='user-list'),
+	url(r'users/(?P<pk>[0-9]+)/', views.UserDetail.as_view(), name='user-detail'),
+])
 
-]
-
-# User URLs
-urlpatterns += [
-	url(r'^users/$', views.UserList.as_view()),
-	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-]
 
 """
 The r'^api-auth/' part of pattern can actually be whatever URL you want to use.
@@ -22,11 +21,9 @@ Only restriction is that the included URLs must have 'rest_framework' namespace
 This will add the ability to login to a browsable API.
 """
 
-# API URLs
+# Login and Logout views for the browsable API.
 urlpatterns += [
 	url(r'api-auth/', include('rest_framwork.urls',
 				 	namespace='rest_framework'))	
 ]
 
-# Create format suffixes patterns for our URLs
-urlpatterns = format_suffix_patterns(urlpatterns)
