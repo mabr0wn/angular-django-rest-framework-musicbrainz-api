8dcf30b5731ce9c63d0e82cd7fbdf27f2439e567
Matthew Brown <mattd429@gmail.com>
2017-10-18 10:03:19 -0400

    Update views.py
    added reverse

--- experiments/views.py
+++ experiments/views.py
@@ -6,6 +6,7 @@ from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.decarators import api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework import renderers
 
 """
