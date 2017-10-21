33e6d02cfd0aa75a6e1d0c068cda53fe6ab18a96
Matthew Brown <mattd429@gmail.com>
2017-10-21 17:55:37 -0400

    Update models.py
    we are adding two models in our collections project folder, we will basically need to to communicate to our experiment models.  so we can create some models to communicate to our MusicBrainzngs server.

--- collections/models.py
+++ collections/models.py
@@ -1,1 +1,27 @@
+from django.db import models.
 
+''' Create a model class Collection '''
+class Collection(models.Model):
+    name = models.CharField(max_length=255)
+    creator = models.CharField(max_length=255)
+    slug = models.SlugField()
+    
+    class Meta:
+        ordering = ['name']
+        
+    def __str__(self):
+        return self.name
+      
+''' Create a model class Record '''
+class Record(models.Model):
+    name = models.CharField(max_length=255)
+    collection = models.ForeignKey(Collection)
+    record_number = models.PostiveIntegerField(blank=True, null=True)
+    
+    class Meta:
+        ordering = ['collection', 'record_number']
+    
+    def __str__(self):
+        return self.name
+    
+    
