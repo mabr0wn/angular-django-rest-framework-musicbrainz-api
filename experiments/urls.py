1d9ea49c5371e7c93a498eefb7001e53edcaa2e4
Matthew Brown <mattd429@gmail.com>
2017-10-24 11:05:37 -0400

    Update urls.py

--- experiments/urls.py
+++ experiments/urls.py
@@ -10,15 +10,22 @@ from views, no longer required.
 router = routers.SimpleRouter()
 router.register(r'experiments', views.ExerimentsViewSet)
 router.register(r'users', views.UserViewSet)
-router.register(r'musicians', views.MusicianViewSet)
+router.register(r'musicians', MusicianViewSet)
 router.register(r'collections', CollectionViewSet)
 router.register(r'records', RecordViewSet)
+
+schema_view = get_schema_view(title='Pastebin API')
 
 """
 The API URLs are now determined automatically by the router.
 Additionally, we include the login URLs for the browsable API.
 """
 urlpatterns = [
-	url(r'^', include(router.urls)),
+	url(r'^api/', include(router.urls)),
 	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+	url(r'^schema/$', schema_view),
+	
+	url(r'^recordings/(?P<album>[\w-]+)/(?P<track>[\w-]+)/(?P<artist>[\w-]+)/$',
+    	views.musician_detail, name='musician_detail_view'),
+	url(r'^$', views.index),
 ]
