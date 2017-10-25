911bd784b7b4856f7e80b1b77fe17497ee3c48dc
Matthew Brown <mattd429@gmail.com>
2017-10-25 10:53:55 -0400

    Update views.py
    creating serializers for our collections and record models.

--- collections/views.py
+++ collections/views.py
@@ -1,1 +1,12 @@
+from rest_framework import viewsets, mixins
 
+from .models import Collection, Record
+from .serializers import CollectionSerializer, RecordSerializer
+
+class CollectionViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    queryset = Collection.objects.all()
+    serializer_class = CollecionSerializer
+   
+class RecordViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
