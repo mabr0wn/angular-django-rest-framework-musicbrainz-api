30f9ce95bf170aadb374cf42d5098473840ad274
Matthew Brown <mattd429@gmail.com>
2017-10-25 18:22:57 -0400

    Update test_serializers.py
    want to test that the serializer adds a slugged version to the data we create for json.

--- experiments/tests/test_serializers.py
+++ experiments/tests/test_serializers.py
@@ -1,1 +1,15 @@
-from
+from unittest import TestCase
+
+from experiments.serializers import MusicianSerializer
+
+class MusicianSerialierTestCase(TestCase):
+  
+    def test_validate(self):
+        ''' Test that MusicianSerializer.validate() adds a slugged version of the artist attribute to the data '''
+        serializer = MusicSerializer()
+        data = serializer.validate({'creator:' 'Trey Songs'})
+        
+        self.assertEqual(data, {
+            'creator': 'Trey Songz',
+            'slug': 'trey-songz'
+        })
