c1771febdae76569cc590f954394e0d76f26b16f
Matthew Brown <mattd429@gmail.com>
2017-10-25 10:49:20 -0400

    Update serializers.py
    serializer so we can serialize the data from our DB and form it into json.  you can also do xml but in our case we need json.

--- collections/serializers.py
+++ collections/serializers.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
 
+from .models import Collection, Record
+
+class CollectionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Collection
+        fields = '__all__'
+        
+class RecordSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Record
+        fields = '__all__'
