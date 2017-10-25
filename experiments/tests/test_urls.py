2958d3ac6478afb880ad9e44bdf92e1bd0272b6c
Matthew Brown <mattd429@gmail.com>
2017-10-25 18:33:23 -0400

    Update test_urls.py
    we are testing to make sure the URLs we produce for music from MB(musicbrianzNGS) is going to the correct URLs we set up for the details.

--- experiments/tests/test_urls.py
+++ experiments/tests/test_urls.py
@@ -1,1 +1,20 @@
+from django.test import TestCase
+from django.core.urlresolvers import resolve
 
+from experiments.views import index
+
+class ExperimentURLsTestCase(TestCase):
+    
+    def test_root_url_uses_index_view(self):
+       ''' Test that the root of the site resolves to the correct view function '''
+        root = resolve('/')
+        self.assertEqual(root.func, index)
+        
+    def test_musician_details_url(self):
+        ''' Test that the URL for MusicianDetail resolves to the correct view function '''
+        musician_detial = resolve('/recordings/lemonade/hold-up/beyoncé/')
+        
+        self.assertEqual(musician_detail.func.__name__, 'musician_detail')
+        self.assertEqual(musician_detail.kwargs['collection'], 'lemonade')
+        self.assertEqual(musician_detail.kwargs['record'], 'hold-up')
+        self.assertEqual(musician_detail.kwargs['creator'], 'beyoncé')
