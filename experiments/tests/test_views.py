d5d1eda84ee00886aaf139c6024cf584666154cf
Matthew Brown <mattd429@gmail.com>
2017-10-25 11:29:16 -0400

    Update test_views.py

--- experiments/tests/test_views.py
+++ experiments/tests/test_views.py
@@ -1,8 +1,23 @@
-import djago.test import TestCase
-from .models import Experiment
-from .views import ExperimentList, ExperimentDetail
+from unittest.mock import patch, Mock
 
-class ExperimentTestCase(TestCase):
-    def setup(self):
-        pass
+from django.test import TestCase, RequestFactory
+from django.db.models.query import QuerySet
+
+from experiments.views import index, musician_detail
+from experiments.models import Musician
+from collections.models import Collection, Record
+
+class MusicianBaseTestCase(TestCase):
     
+    def setUp(self):
+        # Returns a request
+        self.factory = RequestFactory()
+        
+        @classmethod
+        def setUpClass(cls):
+            super().setUpClass()
+            cls.voices = Collection.objects.create(name='Voices', slug='voices')
+            cls.nothing_but_trouble = Record.objects.create(name='Nothing but Trouble', slug='nothing-but-trouble', album=cls.voices)
+            cls.electropop_musician = Musician.objects.create(genre='electropop', artist='Phantogram', track=cls.nothing_but_trouble,
+                                                              slug='phantogram')
+            
