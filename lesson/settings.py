bec10f9b82268afc8fba325f92687048776960ad
Matthew Brown <mattd429@gmail.com>
2017-10-25 18:54:38 -0400

    Update settings.py

--- lesson/settings.py
+++ lesson/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'experiments.apps.ExperimentsConfig',
+    'collections.apps.CollectionsCongig',
 ]
 
 MIDDLEWARE = [
