f604d808dcc161c772f272a44478c079fa8fde20
Matthew Brown <mattd429@gmail.com>
2017-10-31 15:56:18 -0400

    Update tests.py

--- lesson/tests.py
+++ lesson/tests.py
@@ -15,13 +15,23 @@ class StudentTestCase(LiveServerTestCase):
         
         self.colletion1 = Collection.objects.create(name='The Fame', slug='the-fame')
         self.record1 = Record.objects.create(name='Just Dance', slug='just-dance')
-        self.musician1 = Musician.objects.create(genre='pop', artist='Lady Gag', track=self.track1,
+        self.musician1 = Musician.objects.create(genre='pop', artist='Lady Gag', record=self.record1,
                                                  slug='lady-gaga')
         self.collection2 = Collection.objects.create(name='Voices', slug='voices')
         self.record2 = Record.objects.create(name='Black Out Days', slug='black-out-days')
-        self.musician2 = Musician.objects.create(genre='electronic', artist='Phantogram', track=self.track2,
+        self.musician2 = Musician.objects.create(genre='electronic', artist='Phantogram', record=self.record2,
                                                  slug='phantogram')
         self.collection3 = Collection.objects.create(name='Late Registration', slug='late-registration')
         self.record3 = Record.objects.create(name='I Need to Know', slug='i-need-to-know')
-        self.musicians3 = Musician.objects.create(genre='hiphop', artist='Kanye West', track=self.track3,
+        self.musicians3 = Musician.objects.create(genre='hiphop', artist='Kanye West', record=self.record3,
                                                  slug='kayne-west')
+        
+        self.musician4 = Musician.objects.create(genre='electropop', artist='Gorillaz', record=self.track2,
+                                                 slug='gorillaz', start_time='0:25', end_time='3:47')
+        record4 = Record.objects.create(name='My Only Friend', collection=self.collection2, record_number=11)
+        record5 = Record.objects.create(name='Never Going Home', collection=self.collection2, record_number=4)
+        
+        self.admin_user = get_user_model().objects.create_superuser(username='username', email='example@example.com'
+                                                                    password='password')
+        
+        
