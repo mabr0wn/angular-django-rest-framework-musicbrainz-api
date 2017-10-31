1fb6679c4a28a27433f37862c4ce6fe4a03b8bc0
Matthew Brown <mattd429@gmail.com>
2017-10-31 15:06:46 -0400

    Update create_superuser.py
    created a function to create a superuser.

--- experiments/commands/create_superuser.py
+++ experiments/commands/create_superuser.py
@@ -1,1 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth imort get_user_model
+from django.db.utils import IntegrityError
 
+class Command(BaseCommand):
+  help 'Creates a superuse with the default password'
+  
+  def add_arguments(self, parser):
+      """
+      Adds 'username' as a required argument
+      paser allows you to edit this argument
+      with username or email as a string
+      """
+      parser.add_argument('username', type=str)
+      parser.add_argument('email', type=str)
+      
+  def handle(self, *args, **kwargs):
+      """
+      Creates a superuser
+      """
+      try:
+          user = get_user_model().objects.create_superuser(
+              username=options['username'],
+              email=options['email'],
+              password='membership'
+          )
+      except IntegrityError:
+          return self.stdout.write('User' \'{}\' already exists'.format(options['username']))
+      self.stdout.write('User \'{}\' created with default password'.format(user.username))
