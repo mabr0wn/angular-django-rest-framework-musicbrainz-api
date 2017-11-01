37b142ae009e544ca56ee41fd8065ee6e4644ae1
Matthew Brown <mattd429@gmail.com>
2017-11-01 10:21:31 -0400

    Update create_superuser.py

--- experiments/commands/create_superuser.py
+++ experiments/commands/create_superuser.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth imort get_user_model
+from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
 
 class Command(BaseCommand):
