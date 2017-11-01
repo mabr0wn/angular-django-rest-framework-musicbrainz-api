745ad14cef05d7beeb0ba17c9f71bd69fc2ac7c8
Matthew Brown <mattd429@gmail.com>
2017-11-01 18:07:49 -0400

    Update local.py

--- lesson/settings/local.py
+++ lesson/settings/local.py
@@ -1,1 +1,23 @@
+from .base import *
 
+import os
+
+SECRET_KEY = 'DJANGO_SECRET_KEY'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'musicbrainz_db',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
