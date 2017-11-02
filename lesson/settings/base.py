6e54290cb8f84e9907483ef40140b65f9af9f292
Matthew Brown <mattd429@gmail.com>
2017-11-02 13:28:50 -0400

    Update base.py

--- lesson/settings/base.py
+++ lesson/settings/base.py
@@ -1,1 +1,35 @@
+import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+### Celery
+# not ready yet, in localhost program
+
+''' This will allow you to transfer message to one server to another.
+BROKER_URL = 'redis://localhost'
+CELERY_RESULT_BACKEND = 'redis://locahost'
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'experiments',
+    'collections',
+    'rest_framework',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
