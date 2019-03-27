3ed43593bea90ff4fbb4f93be8a6e7a596f4c475
Matt Brown <mattd429@gmail.com>
2019-03-26 22:38:16 -0400

    testing django with angular

--- angular-musicbrainz-api/src/app/app-routing.module.ts
+++ angular-musicbrainz-api/src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes: Routes = [
-  {
-    // Null routes...
-  }
+// const appRoutes: Routes = [
+//   {
+//     // Null routes...
+//   }
 
-]
+// ]
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(
-      appRoutes
-    )
+    // RouterModule.forRoot(
+    //   appRoutes
+    // )
   ],
   exports: [
     RouterModule
