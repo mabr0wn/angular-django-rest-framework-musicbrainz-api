bd8c8930985b0367c926499e706062020226d24c
Matt Brown <mattd429@gmail.com>
2019-03-26 23:14:46 -0400

    webpack working with custom webpack

--- angular-musicbrainz-api/custom-webpack.config.js
+++ angular-musicbrainz-api/custom-webpack.config.js
@@ -1,7 +1,19 @@
 const BundleTracker = require('webpack-bundle-tracker');
+const path = require('path');
+
 
 module.exports = {
-
+    output: {
+        "path":  path.join(process.cwd(), "dist"),
+        "filename":  "[name].bundle.js",
+        "chunkFilename":  "[id].chunk.js",
+        "crossOriginLoading":  false,
+        "publicPath":"http://127.0.0.1:4200/"//1
+    },
+    "devServer": {
+        "historyApiFallback":  true,
+        "publicPath":  "http://127.0.0.1:4200/",//2
+    },
     plugins: [
         new BundleTracker({ filename: '../webpack-stats.json' })
     ]
