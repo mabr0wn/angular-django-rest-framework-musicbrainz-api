0e0fac9fa69a1e50bf660eec239d4d11a731e46b
Matt Brown <mattd429@gmail.com>
2019-03-26 22:09:08 -0400

    app-routing

---
+++ angular-musicbrainz-api/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    // Null routes...
+  }
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ]
+})
+export class AppRoutingModule { }
