2cb439ed9f8338439ceb4eb87faeb4ec0c85f3b5
Matt Brown <mattd429@gmail.com>
2019-04-03 23:08:25 -0400

    [tests][selenium] fixed selenium browser testing, for now using place holder for google.com

--- django-rest-framework-api/lesson/tests.py
+++ django-rest-framework-api/lesson/tests.py
@@ -2,6 +2,7 @@ from django.test import LiveServerTestCase
 # Not guarentee that the user model is loaded into app cache.
 from django.contrib.auth import get_user_model
 
+import time
 from selenium import webdriver
 
 from experiments.models import Musician
@@ -10,26 +11,27 @@ from assortment.models import Assortment, Record
 class UserTestCase(LiveServerTestCase):
     ''' Tell the webdriver to poll the Document object model to chrome and wait 2 seconds '''
     def setUp(self):
-        self.browser = webdriver.Chrome()
-        self.browser.implicity_wait(2)
+        self.browser = webdriver.Chrome('/Users/matthewbrown/downloads/chromedriver')
+        self.browser.get('http://www.google.com/xhtml')
+        time.sleep(5) # Let the user actually see something!
         
-        self.colletion1 = Collection.objects.create(name='The Fame', slug='the-fame')
+        self.colletion1 = Assortment.objects.create(name='The Fame', slug='the-fame')
         self.record1 = Record.objects.create(name='Just Dance', slug='just-dance')
         self.musician1 = Musician.objects.create(genre='pop', artist='Lady Gag', record=self.record1,
                                                  slug='lady-gaga')
-        self.collection2 = Collection.objects.create(name='Voices', slug='voices')
+        self.assortment2 = Assortment.objects.create(name='Voices', slug='voices')
         self.record2 = Record.objects.create(name='Black Out Days', slug='black-out-days')
         self.musician2 = Musician.objects.create(genre='electronic', artist='Phantogram', record=self.record2,
                                                  slug='phantogram')
-        self.collection3 = Collection.objects.create(name='Late Registration', slug='late-registration')
+        self.assortment3 = Assortment.objects.create(name='Late Registration', slug='late-registration')
         self.record3 = Record.objects.create(name='I Need to Know', slug='i-need-to-know')
         self.musicians3 = Musician.objects.create(genre='hiphop', artist='Kanye West', record=self.record3,
                                                  slug='kayne-west')
         
         self.musician4 = Musician.objects.create(genre='electropop', artist='Gorillaz', record=self.track2,
                                                  slug='gorillaz', start_time='0:25', end_time='3:47')
-        record4 = Record.objects.create(name='My Only Friend', collection=self.collection2, record_number=11)
-        record5 = Record.objects.create(name='Never Going Home', collection=self.collection2, record_number=4)
+        record4 = Record.objects.create(name='My Only Friend', assortment=self.assortment2, record_number=11)
+        record5 = Record.objects.create(name='Never Going Home', assortment=self.assortment2, record_number=4)
         
         self.admin_user = get_user_model().objects.create_superuser(username='username', email='example@example.com',
                                                                     password='password')
