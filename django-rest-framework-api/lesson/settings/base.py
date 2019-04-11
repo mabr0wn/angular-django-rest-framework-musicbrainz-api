52405118ac30829f69027c023c14a281577985df
Matt Brown <mattd429@gmail.com>
2019-04-11 17:53:45 -0400

    updated whitenoise

--- django-rest-framework-api/lesson/settings/base.py
+++ django-rest-framework-api/lesson/settings/base.py
@@ -2,7 +2,6 @@ import os
 import dj_database_url
 
 BASE_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..'))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 
@@ -133,13 +132,13 @@ USE_TZ = True
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
+ 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
 
 # Simplified static file serving.
