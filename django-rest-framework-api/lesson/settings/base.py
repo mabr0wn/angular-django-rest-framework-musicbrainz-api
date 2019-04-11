815e752bd9fdbad28e7c4e9d32475477d0028795
Matt Brown <mattd429@gmail.com>
2019-04-11 19:29:31 -0400

    removed .pyc

--- django-rest-framework-api/lesson/settings/base.py
+++ django-rest-framework-api/lesson/settings/base.py
@@ -1,5 +1,6 @@
 import os
-import dj_database_url
+import sys
+print(sys.executable)
 
 BASE_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
@@ -129,6 +130,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Update database configuration with $DATABASE_URL.
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
