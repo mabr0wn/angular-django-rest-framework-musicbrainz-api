4c49e6979a0a6c7f3bff0e22917794c0b1be65ed
Matt Brown <mattd429@gmail.com>
2019-04-12 14:42:12 -0400

    added quick search bar google like searchbar using material

--- angular-musicbrainz-api/src/app/app.module.ts
+++ angular-musicbrainz-api/src/app/app.module.ts
@@ -1,19 +1,31 @@
 // Angular
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MatAutocompleteModule } from '@angular/material';
 // Local
 import { AppComponent } from './views/app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SearchComponent } from './search/search.component';
+import { BoldPipe } from './search/bold.pipe';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    BoldPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-musicbrainz-api/src/app/search/search.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { SAMPLE_RESULTS } from '../sample-results';
+import { map, startWith } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+
+  searchControl: FormControl;
+
+  filteredResults$: Observable<string[]>;
+
+  results = SAMPLE_RESULTS;
+
+  constructor() {
+    this.searchControl = new FormControl('');
+    this.filteredResults$ = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      map(val => this.filterResults(val)),
+      map(val => val.slice(0, 4)));
+  }
+
+  private filterResults(val: string): string[] {
+    return val ? this.results.filter(v => v.toLowerCase().indexOf(val.toLowerCase()) === 0) : [];
+  }
+
+}
