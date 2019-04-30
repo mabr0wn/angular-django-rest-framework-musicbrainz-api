719a7eadc0af17601895626a3f7612254c521cd5
Matt Brown <mattd429@gmail.com>
2019-04-29 23:17:13 -0400

    need to rewrite test urls

--- django-rest-framework-api/artists/tests/test_urls.py
+++ django-rest-framework-api/artists/tests/test_urls.py
@@ -1,12 +1,12 @@
-# # Django
-# from django.test import TestCase
-# from django.urls import resolve
-# # Local
-# from artists.views import Index
+# Django
+from django.test import TestCase
+from django.urls import resolve
+# Local
+from artists.views import Index
 
-# class ExperimentURLsTestCase(TestCase):
+class ExperimentURLsTestCase(TestCase):
     
-#     def test_root_url_uses_index_view(self):
-#        ''' Test that the root of the site resolves to the correct view function '''
-#     root = resolve('/')
-#     self.assertEqual(root.func, Index)
+    def test_root_url_uses_index_view(self):
+       ''' Test that the root of the site resolves to the correct view function '''
+       root = resolve('/')
+       self.assertEqual(root.func, Index)
