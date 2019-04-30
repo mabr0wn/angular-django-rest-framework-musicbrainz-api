947d8f8b320f7d8761df0644201e2b7db80a1108
Matt Brown <mattd429@gmail.com>
2019-04-29 23:12:32 -0400

    test_serialiers

--- django-rest-framework-api/artists/tests/test_serializers.py
+++ django-rest-framework-api/artists/tests/test_serializers.py
@@ -1,16 +1,15 @@
-# # Mock
-# from unittest import TestCase
-# # Local
-# from artists.serializers import ArtistSerializer
+# Mock
+from unittest import TestCase
+# Local
+from artists.serializers import ArtistSerializer
 
-# class ArtistSerialierTestCase(TestCase):
+class ArtistSerialierTestCase(TestCase):
   
-#     def test_validate(self):
-#         ''' Test that ArtistSerializer.validate() adds a slugged version of the artist attribute to the data '''
-#         serializer = ArtistSerializer()
-#         data = serializer.validate({'creator:' 'Trey Songs'})
+    def test_validate(self):
+        ''' Test that ArtistSerializer.validate() adds a slugged version of the artist attribute to the data '''
+        serializer = ArtistSerializer()
+        data = serializer.validate({'artist': 'Trey Songz'})
         
-#         self.assertEqual(data, {
-#             'creator': 'Trey Songz',
-#             'slug': 'trey-songz'
-#         })
+        self.assertEqual(data, {
+            'artist': 'Trey Songz',
+        })
