7d8523dddb7e305e2dd97799da27a0495b14161b
Matt Brown <mattd429@gmail.com>
2019-05-02 17:44:13 -0400

    pytet testing serializer

--- django-rest-framework-api/artists/tests/test_serializers.py
+++ django-rest-framework-api/artists/tests/test_serializers.py
@@ -1,15 +1,24 @@
+# Pytest
+import pytest
 # Mock
 from unittest import TestCase
 # Local
-from artists.serializers import ArtistSerializer
+from ..models import Track, Album, Artist
+from ..serializers import TrackSerializer, AlbumSerializer, ArtistSerializer
+from .data_prep import data_prep
 
-class ArtistSerialierTestCase(TestCase):
-  
-    def test_validate(self):
-        ''' Test that ArtistSerializer.validate() adds a slugged version of the artist attribute to the data '''
-        serializer = ArtistSerializer()
-        data = serializer.validate({'artist': 'Trey Songz'})
-        
-        self.assertEqual(data, {
-            'artist': 'Trey Songz',
-        })
+# We need to do this so that writing to the DB is possible in our tests.
+pytestmark = pytest.mark.django_db
+
+class ArtistSerializerTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        data_prep.create_()
+        cls.expected_artists = data_prep.expected_artists()
+
+    def test_all_fields_in_serialized_artist(self):
+        artist = Artist.objects.create(artist='test_artist_1')
+        Artist.objects.get(artist='test_artist_1')
+        serialized = ArtistSerializer(artist)
+        self.assertEqual(serialized.data, self.expected_artists[0])
