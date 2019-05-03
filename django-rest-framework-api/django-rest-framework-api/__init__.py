25bc9de902d3b9fdf00fba942e1e9d4b7cb0bc7c
Matt Brown <mattd429@gmail.com>
2019-05-03 13:36:05 -0400

    celery comments

--- django-rest-framework-api/django-rest-framework-api/__init__.py
+++ django-rest-framework-api/django-rest-framework-api/__init__.py
@@ -1,10 +1,1 @@
-"""
-Let's break down what happens in the first module,
-first we import absolute imports from the future,
-so that our celery.py module won't clash with the library
-"""
-
-from __future__ import absolute_import, unicode_literals
-# This will make sure the app is always import when
-# Django starts so that shared_task decorator will use this app.
 from .celery import app as celery_app
