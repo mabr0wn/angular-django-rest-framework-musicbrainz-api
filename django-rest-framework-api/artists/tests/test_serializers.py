bce563006c33a92d663ea89ce8c9b6c79e8ad771
Matt Brown <mattd429@gmail.com>
2019-05-02 22:13:15 -0400

    pytest passing

--- django-rest-framework-api/artists/tests/test_serializers.py
+++ django-rest-framework-api/artists/tests/test_serializers.py
@@ -3,22 +3,17 @@ import pytest
 # Mock
 from unittest import TestCase
 # Local
-from ..models import Track, Album, Artist
-from ..serializers import TrackSerializer, AlbumSerializer, ArtistSerializer
-from .data_prep import data_prep
+from ..serializers import ArtistSerializer
 
 # We need to do this so that writing to the DB is possible in our tests.
 pytestmark = pytest.mark.django_db
 
 class ArtistSerializerTest(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        data_prep.create_()
-        cls.expected_artists = data_prep.expected_artists()
+    def test_validate(self):
+        serializer = ArtistSerializer()
+        data = serializer.validate({'artist': 'Ray Brown'})
 
-    def test_all_fields_in_serialized_artist(self):
-        artist = Artist.objects.create(artist='test_artist_1')
-        Artist.objects.get(artist='test_artist_1')
-        serialized = ArtistSerializer(artist)
-        self.assertEqual(serialized.data, self.expected_artists[0])
+        self.assertEqual(data, {
+            'artist': 'Ray Brown',
+        })
