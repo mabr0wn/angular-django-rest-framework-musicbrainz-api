77fbcc1528747526d5e18a874d764088c23606a9
Matt Brown <mattd429@gmail.com>
2019-05-04 21:08:58 -0400

    modify view

--- django-rest-framework-api/artists/views.py
+++ django-rest-framework-api/artists/views.py
@@ -1,13 +1,38 @@
 # Django rest
 from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework import filters
+from rest_framework.exceptions import NotFound
+
 # Local
 from artists.models import Artist, Album, Track
 from artists.serializers import ArtistSerializer, AlbumSerializer, TrackSerializer
+from .tasks import get_artist_tracks_from_musicbrianz_api
 
 
-class ArtistViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
+class ArtistViewSet(viewsets.ModelViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('artist',  'genre')
+
+    @action(methods=['post'], detail=True)
+    def get_queryset(self, **kwargs):
+        artists = []
+
+        text = kwargs.get('text', None)
+        
+        if text:
+            artists = Artist.objects.filter(artist=text)
+
+        if not artists:
+           artist = get_artist_tracks_from_musicbrianz_api(text)
+           artists = [artist]
+        return artists
+
+
+
+
 
 class AlbumViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
     queryset = Album.objects.all()
