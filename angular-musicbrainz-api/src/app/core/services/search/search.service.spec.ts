bc8d7dfb5d135a83db494d821d50d4bea01fa586
Matt Brown <mattd429@gmail.com>
2019-05-05 22:29:42 -0400

    before jest-codemods

--- angular-musicbrainz-api/src/app/core/services/search/search.service.spec.ts
+++ angular-musicbrainz-api/src/app/core/services/search/search.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 
@@ -10,42 +10,35 @@ import { ErrorHanlderService } from '../error-handler/error-hanlder.service';
 import { Album } from '../../models/album';
 
 describe('SearchService', () => {  
-  let service: SearchService;
-  let http: HttpClient;
-  let spy: any;
+  let getSpy: jasmine.Spy;
 
   beforeEach(() => {
+    const http = jasmine.createSpyObj('HttpClient', ['get']);
+    getSpy = http.get;
+
     TestBed.configureTestingModule({
-      providers:[
+      providers: [
         SearchService,
-        { provide: HttpClient, useValue: { get: jest.fn() } },
-        {provide: ErrorHanlderService }
+        ErrorHanlderService,
+        {provide: HttpClient, useValue: http}
       ]
-    })
-    http = TestBed.get(HttpClient);
-    service = TestBed.get(SearchService);
-
-    // Mock implementation of console.error to
-    // return undefined to stop printing out to console log during test
-    jest.spyOn(console, 'error').mockImplementation(() => undefined)
+    });
   });
-
-  it('should create an instance successfully', () => {
-    expect(service).toBeDefined();
-  });
-
-  describe('',() => {
+  describe('"searchAlbums" method', () => {
     it('calls external api with supplied query value and field', inject([SearchService], (service: SearchService) => {
-      expect(http.get).not.toHaveBeenCalled();
+      expect(getSpy).not.toHaveBeenCalled();
 
       let argUrl: string;
       let argOptions: {params: HttpParams};
-      spy = spyOn(service,'searchAlbums').and.callFake(
+      getSpy.and.callFake(
         (url, options) => { argUrl = url; argOptions = options; return of([])
       });
       const testValue = 'testValue';
       const testField = 'testField';
       service.searchAlbums(testValue, testField).subscribe();
-    }))
+
+      expect(getSpy).toHaveBeenCalledTimes(1);
+      expect(argUrl).toContain('https://musicbrainz.org/');
+    }));
   })
 });
