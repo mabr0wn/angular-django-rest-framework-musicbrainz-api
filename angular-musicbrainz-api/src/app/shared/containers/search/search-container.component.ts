f6c0a8dbcb6f08b4d337f4fce4aa7733d688a580
Matt Brown <mattd429@gmail.com>
2019-05-09 14:58:15 -0400

    removed variables and methods until we will need to implement them in the search container

--- angular-musicbrainz-api/src/app/shared/containers/search/search-container.component.ts
+++ angular-musicbrainz-api/src/app/shared/containers/search/search-container.component.ts
@@ -37,41 +37,28 @@ export class SearchContainer implements OnInit {
 
   // search vars
   queryString: string;
-  searching: boolean;
   searchType: string;
-
-
   searchTerms: Subject<SearchParams> = new Subject<SearchParams>();
-  results$: Observable<Album[]>;
 
   private ngUnsubscribe: Subject<any> = new Subject();
 
   constructor(private searchService: SearchService) { }
 
+  // Not tested yet...
   ngOnInit(): void {
-    this.searching = false;
     this.searchType = 'release';
     this.searchTerms.pipe(
       distinctUntilChanged((params1, params2) => params2.equals(params1)),
       switchMap(
-        (params) => this.searchService.searchAlbums(params.term, params.type),
-        (_params, albums, _outerIdx, _innerIdx) => of(albums)
-       )
+        (params) => this.searchService.searchAlbums(params.term, params.type))
     ).subscribe((albums) => {
       console.log(albums);
-      this.results$ = albums;
-      this.searching = false;
     });
   }
 
   // Push a search term into the observable stream.
+  // Not Tested...
   search(): void {
     this.searchTerms.next(new SearchParams(this.queryString, this.searchType));
-  }
-
-  filterResults(title: string) {
-    return title ? this.results$.pipe(
-      map(val => val.filter(v => v.title.indexOf(title) === 0))
-    ) : [];
   }
 }
