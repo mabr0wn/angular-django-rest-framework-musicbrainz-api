2c0560d6581ea3494da9c07850fa01a19e0de216
Matt Brown <mattd429@gmail.com>
2019-05-12 17:52:51 -0400

    [term] => this presenter spec is passing

--- angular-musicbrainz-api/src/app/shared/search/presenter/search.presenter.spec.ts
+++ angular-musicbrainz-api/src/app/shared/search/presenter/search.presenter.spec.ts
@@ -1,10 +1,39 @@
 import { fakeAsync,  tick } from '@angular/core/testing';
+import { Subscription } from 'rxjs';
+
 
 import { SearchPresenter } from './search.presenter';
 
-describe('SearchPresenterComponent', () => {
-    test('', () => {
-        expect('').toEqual('')
-    })
+describe(SearchPresenter.name, () => {
+    let presenter: SearchPresenter;
 
-});
+
+    beforeEach(() => {
+        presenter = new SearchPresenter();
+    });
+
+    describe('emit search terms', () => {
+        let searchTermsSubscription: Subscription;
+        const searchTerm = {
+            searchTerm: jest.fn((term) => `${term}`)
+        };
+
+        beforeEach(() => {
+          searchTermsSubscription = presenter.searchTerms
+            .subscribe(searchTerm.searchTerm);
+        });
+
+        afterEach(() => {
+            searchTermsSubscription.unsubscribe();
+        });
+
+        test('it will descibe an album search', fakeAsync(() => {
+            const tupac = 'tupac';
+
+            presenter.search(tupac);
+
+            expect(searchTerm.searchTerm).toHaveBeenCalledTimes(1);
+            expect(searchTerm.searchTerm).toHaveBeenCalledWith(tupac)
+        }))
+    })
+})
--- angular-musicbrainz-api/src/app/shared/search/presenter/search.presenter.ts
+++ angular-musicbrainz-api/src/app/shared/search/presenter/search.presenter.ts
@@ -6,9 +6,11 @@ import { SearchParams } from '@shared/search/search-params';
 export class SearchPresenter {
   searchTerms: Subject<SearchParams> = new Subject();
 
+
+
   search(term): void {
     // Log the term for debugging purposes
-    console.log(term, '@presenter');
+    // console.log(term, '@presenter');
     this.searchTerms.next(term);
   }
 }
