fb8555a576f40fc04db42954ba650dcb8fad9eba
Matt Brown <mattd429@gmail.com>
2019-05-16 19:59:56 -0500

    model interface for auth token

--- angular-musicbrainz-api/src/app/core/services/auth/auth.service.ts
+++ angular-musicbrainz-api/src/app/core/services/auth/auth.service.ts
@@ -5,6 +5,14 @@ import { Router } from '@angular/router';
 // RxJS
 import { Observable, Subject} from 'rxjs';
 
+interface AuthResponse {
+  token: string
+}
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
