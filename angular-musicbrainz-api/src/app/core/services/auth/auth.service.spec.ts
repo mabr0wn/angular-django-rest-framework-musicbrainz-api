d23f20ebe2185c3bc09ecca2d54e35a9cbba35f5
Matt Brown <mattd429@gmail.com>
2019-05-18 10:06:24 -0500

    testing auth service for fake login passing.

--- angular-musicbrainz-api/src/app/core/services/auth/auth.service.spec.ts
+++ angular-musicbrainz-api/src/app/core/services/auth/auth.service.spec.ts
@@ -1,10 +1,12 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
+  let authService: AuthService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -13,9 +15,27 @@ describe('AuthService', () => {
       ],
       providers: [AuthService]
     });
+
+    authService = TestBed.get(AuthService);
   });
 
-  it('should be created', inject([AuthService], (service: AuthService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('Login', () => {
+    test('Should return user information', fakeAsync(() => {
+      const resquest = authService.logIn({
+        username: 'cheeta',
+        password: '1234'
+      });
+      tick();
+
+      resquest.subscribe(creds => {
+        expect(creds).toBeDefined();
+        expect(creds.token).toBeDefined();
+      });
+    }));
+
+    test('Should authenticate user', fakeAsync(() => {
+      expect('').toEqual('');
+    }))
+  })
+  
 });
