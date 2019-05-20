0c6b28ca864bb36da1c4437d8ae7bfc0950bccd6
Matt Brown <mattd429@gmail.com>
2019-05-20 15:38:08 -0400

    ng lint shared.module.ts

--- angular-musicbrainz-api/src/app/shared/shared.module.ts
+++ angular-musicbrainz-api/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
-  FormsModule, 
+  FormsModule,
   ReactiveFormsModule
 } from '@angular/forms';
 // Local
