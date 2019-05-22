52a564f21686644cf9291bfc333e0a9dc0df34bb
Matt Brown <mattd429@gmail.com>
2019-05-21 22:40:40 -0400

    error spec

--- angular-musicbrainz-api/src/app/core/services/error-handler/error-hanlder.service.spec.ts
+++ angular-musicbrainz-api/src/app/core/services/error-handler/error-hanlder.service.spec.ts
@@ -1,10 +1,15 @@
+import { ErrorHandler, ReflectiveInjector } from '@angular/core';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ErrorHanlderService } from './error-hanlder.service';
 
 describe('ErrorHanlderService', () => {
+  let handler: ErrorHanlderService
+
   beforeEach(() => TestBed.configureTestingModule({
-    providers: [ErrorHanlderService]
+    providers: [
+      { provide: ErrorHandler, useClass: ErrorHanlderService }
+    ]
   }));
 
   it('should be created', inject([ErrorHanlderService], (service: ErrorHanlderService) => {
